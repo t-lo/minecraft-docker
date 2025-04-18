b58864ab732cf1a9c46b0ea971331ab87f7c6aea
Thilo Fromm <thilo.alexander@gmail.com>
2025-04-18 17:33:23 +0200

    Direwolf: update to 1.21.1, fix backup access rights
    Signed-off-by: Thilo Fromm <thilo.alexander@gmail.com>

--- direwolf/build.sh
+++ direwolf/build.sh
@@ -7,14 +7,14 @@ set -euo pipefail
 # for new releases.
 
 # Minecraft 1.21.1
-#pack=126
-#version=100062
-#release="minecraft-1.21.1-neoforge-21.1.145-direwolf-1.12.0"
+pack=126
+version=100062
+release="minecraft-1.21.1-neoforge-21.1.145-direwolf-1.12.0"
 
 # Minecraft 1.20.1
-pack=119
-version=100051
-release="minecraft-1.20.1-forge-47.1.84-direwolf-1.16.1"
+#pack=119
+#version=100051
+#release="minecraft-1.20.1-forge-47.1.84-direwolf-1.16.1"
 
 docker build --build-arg pack_id="${pack}" \
              --build-arg version_id="${version}" -t direwolf:${release} .
